2db0e415d8041059682d7e4abe3bb784a1ee2597
Estefania Oña <jennifere.ona@ucuenca.edu.ec>
2021-11-03 15:49:14 -0500

    Carpeta9 Video4

--- shared.js
+++ shared.js
@@ -1,5 +1,11 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-console.dir(backdrop);
-
-backdrop.style.display = 'block';
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
